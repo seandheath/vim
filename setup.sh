d1d92a5992c3b5e4aba5d185e16507aa0dc1b282
user <user@localhost.localdomain>
2019-02-04 11:38:20 -0500

    removed nvim symbolic link

--- setup.sh
+++ setup.sh
@@ -3,4 +3,3 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/vimrc ~/.config/nvim/init.vim
