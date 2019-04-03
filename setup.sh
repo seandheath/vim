444152bd2ae6ce0cbc54c14bcf89e5a1d8226b1f
user <user@pop-os.localdomain>
2019-04-03 10:17:36 -0400

    updated install script to rerun without error

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+rm ~/.vimrc
 ln -s ~/.vim/vimrc ~/.vimrc
 mkdir ~/.config/nvim
+rm ~/.config/init.vim
+rm -rf ~/.config/nvim/autoload
 ln -s ~/.vim/vimrc ~/.config/nvim/init.vim
 ln -s ~/.vim/autoload/ ~/.config/nvim/autoload
 vim +PlugInstall +qall > /dev/null
